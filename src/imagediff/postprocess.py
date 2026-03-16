18466b1e675f3bccd8b7798f36fb5c34174760db
Iver Oknes <iver@oknes.no>
2026-03-16 17:47:20 +0000

    Update variable names for path arguments

--- src/imagediff/postprocess.py
+++ src/imagediff/postprocess.py
@@ -1,9 +1,8 @@
 import os
+
 import numpy as np
 from cv2 import imread, imwrite
 
-def load_images(regex: str):
-    pass
 
 def load_folder(folder_path: str):
     images = []
@@ -25,25 +24,27 @@ def image_diff(ref_image: np.ndarray, image: np.ndarray):
     return diff.astype(np.uint8)
 
 
-def postprocess(ref_image: str, image: str, output: str):
-    ref_image = imread(ref_image)
-    image = imread(image)
+def postprocess(ref_image_path: str, image_path: str, output_path: str):
+    ref_image = np.array(imread(ref_image_path))
+    image = np.array(imread(image_path))
 
-    if not output.endswith(".tif"):
-        output += ".tif"
+    if not output_path.endswith(".tif"):
+        output_path += ".tif"
 
     diff = image_diff(ref_image, image)
-    imwrite(output, diff)
+    imwrite(output_path, diff)
 
 
-def postprocess_folder(ref_image: str, image: str, output: str):
-    ref_image = imread(ref_image)
-    images = load_folder(image)
+def postprocess_folder(
+    ref_image_path: str, image_folder_path: str, output_folder_path: str
+):
+    ref_image = np.array(imread(ref_image_path))
+    images = load_folder(image_folder_path)
 
-    if not os.path.exists(output):
-        os.makedirs(output)
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
 
     diffs = [image_diff(ref_image, i) for i in images]
 
     for i, diff in enumerate(diffs):
-        imwrite(os.path.join(output, f"diff_{i}.png"), diff)
+        imwrite(os.path.join(output_folder_path, f"diff_{i}.png"), diff)
